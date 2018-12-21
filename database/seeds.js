1e8dbe99efb080280279b7c83b37df64661e7ce5
Liam Williams <liamwilliams680@gmail.com>
2018-12-20 17:06:23 -0800

    Seeding Database

--- database/seeds.js
+++ database/seeds.js
@@ -1,7 +1,6 @@
 const db = require('../database');
 const faker = require('faker');
 
-<<<<<<< HEAD
 let getRandomInt = (max) => {
   return Math.floor(Math.random() * Math.floor(max));
 };
@@ -21,9 +20,3 @@ for (let i = 0; i < 100; i++) {
   db.query(`INSERT INTO restaurants (name, address, picture, stars, quality, delivery, accuracy) VALUES ("${companyName}","${address}","${picture}","${stars}", "${quality}", "${delivery}", "${accuracy}");`);
   db.query(`INSERT INTO users (name, email) VALUES ("${name}","${email}");`);
 } 
-=======
-
-for (let i = 0; i < 100; i++) {
-  
-}
->>>>>>> d54100792814de2be7fd8d81d9a4f5f3cf3c3144
