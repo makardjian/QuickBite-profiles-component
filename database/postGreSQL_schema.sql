840a546c085738db5b706fa38454e270ff029899
Michael Kardjian <makardjian@gmail.com>
2019-01-09 19:41:58 -0800

    resave my posgres Schema which got deleted by accident

---
+++ database/postGreSQL_schema.sql
@@ -0,0 +1,20 @@
+-- DROP DATABASE IF EXISTS sdc_project;
+-- CREATE DATABASE sdc_project;
+
+CREATE TABLE restaurants (
+  id bigserial PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  address varchar(255) NOT NULL,
+  number varchar(255) NOT NULL,
+  picture varchar(255) NOT NULL,
+  stars int NOT NULL,
+  quality int NOT NULL,
+  delivery int NOT NULL,
+  accuracy int NOT NULL
+);
+
+CREATE TABLE users (
+  id bigserial PRIMARY KEY,
+  name varchar(255) NOT NULL,
+  email varchar(255) NOT NULL
+);
